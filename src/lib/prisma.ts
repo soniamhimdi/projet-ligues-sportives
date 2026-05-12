e2793a4b395047107e2b7a18cfae2b9f3b559749
asma aj <asma.ajroudi.e@gmail.com>
2026-05-12 17:03:51 -0400

    Mise a jour prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import  PrismaClient } from "@prisma/client";
+import  { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config();
